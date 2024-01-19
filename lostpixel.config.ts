d5abd5f5bac31c590dcafef5c0c57e153a491f93
Nikhil P S <nikhilpsathyanathan@gmail.com>
2024-01-19 16:27:06 +0530

    chore: add package lost-pixel  and config

---
+++ lostpixel.config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  storybookShots: {
+    storybookUrl: "./storybook-static",
+  },
+  // generateOnly: true,
+  lostPixelProjectId: "clrk77ppl013lni0e77nfw1qg",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
